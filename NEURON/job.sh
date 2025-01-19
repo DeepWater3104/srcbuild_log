a216fb8552097a92793c332b786767f60a99e122
DeepWater3104 <fukami.satoshi760@mail.kyutech.jp>
2025-01-19 22:07:18 +0900

    update

--- NEURON/job.sh
+++ NEURON/job.sh
@@ -13,22 +13,22 @@
 
 #. ../../spack/share/spack/setup-env.sh
 . /vol0004/apps/oss/spack/share/spack/setup-env.sh
-#spack load /6mx2lcm # gsl@2.7.1%fj@4.10.0
 spack load /nencizh # gcc@12.2.0
+spack load fujitsu-mpi%gcc@12.2.0
 spack load /fhakchp # python@3.8.12%fj@4.7.0
-#spack load /kndrnlz # py-setuptools@68.0.0%fj4.10.0
-spack load /rn35dtk # py-setuptools@59.4.0%fj4.10.0
+spack load /dgmiy5n # py-numpy@1.25.2%fj@4.10.0
+spack load /kndrnlz # py-setuptools@68.0.0%fj4.10.0
 spack load /37xmpin #py-pip@23.0%fj@4.10.0
-#spack load /wb7zcjt # fujitsu-mpi@head%fj@4.11.1
 spack load /fl35w2u # py-jinja2
 spack load /esj6scy # py-pyyaml
-#spack load /mnavall # py-jinja2
-spack load fujitsu-mpi%gcc@12.2.0
+
 export LD_LIBRARY_PATH=/lib64:$LD_LIBRARY_PATH
 
 spack find --loaded # see the list of loaded modules
 
-which mpifcc mpiFCC
+pip list
+
+which mpicc mpicxx
 
 cmake .. \
   -DNRN_ENABLE_CORENEURON=ON \
@@ -36,12 +36,9 @@ cmake .. \
   -DNRN_ENABLE_INTERVIEWS=OFF \
   -DNRN_ENABLE_RX3D=OFF \
   -DCMAKE_INSTALL_PREFIX=../../install \
-  -DCMAKE_CXX_FLAGS=-lstdc++
-  -DCMAKE_C_COMPILER=gcc \
-  -DCMAKE_CXX_COMPILER=g++ \
-  -DMPI_C_COMPILER=mpifcc \
-  -DMPI_CXX_COMPILER=mpiFCC
+  -DMPI_C_COMPILER=mpicc \
+  -DMPI_CXX_COMPILER=mpicxx
 
-#cmake --build . --parallel 8
-#cmake --build . --target install
-make -j
+cmake --build . --parallel 8
+cmake --build . --target install
+#make -j
